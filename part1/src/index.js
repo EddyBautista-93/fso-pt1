5fa321770923b9c64005879a10f78913dc295e4c
EddyBautista <bautista.j.eddy@gmail.com>
2020-08-16 18:15:52 -0500

    testing out jsx

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// note to self you need to use () not {}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+// const App = () => { 
+//   console.log('Hello from component');
+//   const now = new Date()
+//   const a = 10;
+//   const b = 20;
+
+//   return React.createElement(
+//     'div',
+//     null,
+//     React.createElement(
+//       'p', null, 'Hello world it is ' , now.toString()
+//     ),
+//     React.createElement(
+//       'p',null , a, ' plus ', b, ' is ', a + b
+//     )
+//   )
+// }
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p> Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+   return (
+     <div>
+       <h1> Greetings </h1>
+       <Hello name="Testing Testing one two three" />
+       <Hello name="Testing Testing one two three" />
+       <Hello name="Testing Testing one two three" />
+     </div>
+   )
+}
+
+
+ReactDOM.render( <App />,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
